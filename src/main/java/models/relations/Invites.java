3d691253d917d5e5b7a736e68bd165dfebad889b
Karl Liu <karl.liu.1024@gmail.com>
2019-07-17 02:55:18 +0800

    deleted accepted field

--- src/main/java/models/relations/Invites.java
+++ src/main/java/models/relations/Invites.java
@@ -1,6 +1,5 @@
 package models.relations;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import models.Model;
 import models.entities.Expert;
 import models.entities.Project;
@@ -8,23 +7,21 @@ import models.entities.Project;
 public class Invites extends Model {
     private Project project;
     private Expert expert;
-    private Boolean accepted;
     private String reason;
-
-    public Boolean getAccepted() {
-        return accepted;
-    }
-
-    public void setAccepted(Boolean accepted) {
-        this.accepted = accepted;
-    }
 
     public Invites() {}
 
-    public Invites(Project project, Expert expert, boolean accepted) {
+    public Invites(Project project, Expert expert) {
         this.project = project;
         this.expert = expert;
-        this.accepted = accepted;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
     }
 
     public Project getProject() {
